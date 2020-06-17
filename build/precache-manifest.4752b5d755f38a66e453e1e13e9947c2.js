878c7215e5393d2766a8399820ddc46bf94d8a2d
Saska Rauhala <saska.rauhala@icloud.com>
2020-06-17 13:12:32 +0300

    second commit

---
+++ build/precache-manifest.4752b5d755f38a66e453e1e13e9947c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf713ea9dc416527981f0e454895af82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "459769efa7c06f325c8f",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "ce82040d88d97300f603",
+    "url": "/static/css/main.43cb22d2.chunk.css"
+  },
+  {
+    "revision": "459769efa7c06f325c8f",
+    "url": "/static/js/2.777ace7c.chunk.js"
+  },
+  {
+    "revision": "14223a3f9bfd4e11e3df9b614bac80ff",
+    "url": "/static/js/2.777ace7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce82040d88d97300f603",
+    "url": "/static/js/main.b1a0aacd.chunk.js"
+  },
+  {
+    "revision": "efbb02890f00adb69faf",
+    "url": "/static/js/runtime-main.7fc5b88c.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87789825794ead35955299eed6a933f4.js"
+  "/precache-manifest.4752b5d755f38a66e453e1e13e9947c2.js"
 );
 
 self.addEventListener('message', (event) => {
